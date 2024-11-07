b2ab9eb20496e4b389e6bb0322b1f66709e67076
TETRAKOS <55252269+TETRAKOS@users.noreply.github.com>
2024-11-07 14:19:46 +0800

    *

--- PythonContent/MainClasses/Main.py
+++ PythonContent/MainClasses/Main.py
@@ -8,8 +8,9 @@ surface = pygame.display.set_mode((640,480))
 bgc = (45,48,44)
 running = True
 surface.fill(bgc)
+gameIcon = pygame.image.load("Assets/Sprites/icons/Icon33.png")
 pygame.display.set_caption("Templars of Elysium")
-
+pygame.display.set_icon(gameIcon)
 pygame.display.flip()
 while running:
     for event in pygame.event.get():
